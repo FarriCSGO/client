999c9e9526616ea7510ad5913f8c5e09be6fbdfd
Shikhar Sharma <ceoshikhar@gmail.com>
2020-07-14 22:32:32 +0530

    Add Axios.get for quotes to test the API

--- src/components/home/quotes/quotes.tsx
+++ src/components/home/quotes/quotes.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import Axios from "../../../utils/Axios";
 
 const quotes = () => {
+  Axios.get("/api/quotesByPros")
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+
   return (
     <MainWrapper>
       <h1>Quotes said by CSGO Pros Component</h1>
