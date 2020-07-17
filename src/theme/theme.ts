dccbc29dfcfe305ead0a8c63a8aff742dca5b66a
Shikhar Sharma <ceoshikhar@gmail.com>
2020-07-17 14:47:23 +0530

    Add media query size variables

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,6 +3,11 @@ export const Theme = {
     orange: "#f37335",
     dark: "#0a0a0a",
     light: "#f0f0f0"
+  },
+
+  size: {
+    medium: "only screen and (max-width: 876px) and (min-width: 648px)",
+    small: "only screen and (max-width: 647px)"
   }
 };
 
