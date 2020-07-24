9d0d1c68d79321364ef2ab691cde5d34bd27643f
Shikhar Sharma <ceoshikhar@gmail.com>
2020-07-25 01:08:59 +0530

    Add colors to the dark and light theme

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,8 +1,20 @@
 export const Light = {
   colors: {
-    orange: "#f37335",
-    dark: "#0a0a0a",
-    light: "#f0f0f0"
+    // Primary
+    primary: "#f37335",
+
+    // Background
+    background: "#f0f0f0",
+    background2: "#ffffff",
+
+    // Surface
+    surface: "#ffffff",
+    surface2: "#ffffff",
+
+    // Text
+    textLight: "#ffffff",
+    textFaint: "#888888",
+    textDark: "#0a0a0a"
   },
 
   size: {
@@ -13,9 +25,21 @@ export const Light = {
 
 export const Dark = {
   colors: {
-    orange: "#f37335",
-    dark: "#f0f0f0",
-    light: "#0a0a0a"
+    // Primary
+    primary: "#f37335",
+
+    // Background
+    background: "#121212",
+    background2: "#0D0D0D",
+
+    // Surface
+    surface: "#1B1B1B",
+    surface2: "#202020",
+
+    // Text
+    textLight: "#ffffff",
+    textFaint: "#888888",
+    textDark: "#0a0a0a"
   },
 
   size: {
