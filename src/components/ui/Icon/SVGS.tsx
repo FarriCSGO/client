b6b80202255b127393ef0548c00e5b3206de0e48
Shikhar Sharma <ceoshikhar@gmail.com>
2020-07-26 00:44:15 +0530

    Add a moon svg to navbar. Toggle between moon or sun svg on navbar when the theme pref is changed

--- src/components/ui/Icon/SVGS.tsx
+++ src/components/ui/Icon/SVGS.tsx
@@ -24,6 +24,28 @@ export const SunSVG = () => {
   );
 };
 
+export const MoonSVG = () => {
+  return (
+    <SVG>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="icon icon-tabler icon-tabler-moon"
+        width="40"
+        height="40"
+        viewBox="0 0 24 24"
+        strokeWidth="1.5"
+        stroke="#FF5722"
+        fill="none"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" />
+        <path d="M16.2 4a9.03 9.03 0 1 0 3.9 12a6.5 6.5 0 1 1 -3.9 -12" />
+      </svg>
+    </SVG>
+  );
+};
+
 export const GithubSVG = () => {
   return (
     <svg
