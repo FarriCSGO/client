0d0174116fef03c07a2ef49ca48ad83d7d7f8e81
Shikhar Sharma <ceoshikhar@gmail.com>
2020-08-15 00:40:04 +0530

    remove setTheme action because it is not needed

--- src/core/controller/ui/ui.actions.ts
+++ src/core/controller/ui/ui.actions.ts
@@ -2,24 +2,6 @@ import { THEME, THEME_TYPE } from "./ui.state";
 import { lightTheme } from "./theme/light.theme";
 import { darkTheme } from "./theme/dark.theme";
 
-export const setTheme = (themeName: string) => {
-  switch (themeName) {
-    case "dark":
-      THEME.set(darkTheme);
-      THEME_TYPE.set("dark");
-      break;
-
-    case "light":
-      THEME.set(lightTheme);
-      THEME_TYPE.set("light");
-      break;
-
-    default:
-      THEME.reset();
-      THEME_TYPE.reset();
-  }
-};
-
 export const toggleTheme = () => {
   if (THEME_TYPE.value === "dark") {
     THEME.set(lightTheme);
