c644892f7830d6d10faaef471bf0efe09fe131ba
Shikhar Sharma <ceoshikhar@gmail.com>
2020-08-16 23:24:16 +0530

    Rename api to API

---
+++ src/core/API.ts
@@ -0,0 +1,13 @@
+import { App } from "./app";
+
+let BASE_URL: string;
+
+process.env.NODE_ENV === "production"
+  ? (BASE_URL = "https://farri-server.herokuapp.com")
+  : (BASE_URL = "http://localhost:6969");
+
+export const API = App.API({
+  options: {},
+  baseURL: BASE_URL,
+  path: "api"
+});
